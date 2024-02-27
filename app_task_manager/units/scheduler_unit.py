58cf28b5ca310c3229eabb7e57406b8cafea8106
gil9red <ilya.petrash@inbox.ru>
2024-02-27 23:18:23 +0500

    Обновлено. Добавлены заготовки для новых модулей

---
+++ app_task_manager/units/scheduler_unit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from app_task_manager.units.base_unit import BaseUnit
+
+
+# TODO:
+class SchedulerUnit(BaseUnit):
+    pass
