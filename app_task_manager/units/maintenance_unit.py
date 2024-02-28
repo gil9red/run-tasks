742a9848679229f15ec2f20f44b1ca6a25c70235
gil9red <ilya.petrash@inbox.ru>
2024-02-28 21:01:05 +0500

    Рефакторинг

--- app_task_manager/units/maintenance_unit.py
+++ app_task_manager/units/maintenance_unit.py
@@ -15,56 +15,59 @@ from db import TaskRun, TaskStatusEnum
 
 
 class MaintenanceUnit(BaseUnit):
+    def __processing_hanging_runs(self):
+        task_runs: list[TaskRun] = self.owner.get_current_task_runs()
+        min_start_date: datetime = min(
+            [
+                run.start_date
+                for run in task_runs
+                if run.get_actual_status() == TaskStatusEnum.Running
+            ],
+            default=datetime.now(),
+        )
+        # Небольшая фора
+        min_start_date -= timedelta(seconds=10)
+
+        # Разбирательства с "висячими" запусками
+        for run in TaskRun.select().where(
+            TaskRun.status == TaskStatusEnum.Running,
+            TaskRun.start_date < min_start_date,
+        ):
+            log_prefix = (
+                f"{self._log_prefix} [Задача #{run.task.id}, запуск #{run.id}]"
+            )
+            try:
+                # Попробуем найти процесс, если задан
+                if run.process_id:
+                    p = Process(run.process_id)
+                    process_command: str = " ".join(p.cmdline())
+                    run_command = get_prefix_file_name_command(run.task, run)
+
+                    # Если процесс найден и в аргументах запуска есть часть названия батника
+                    if run_command in process_command:
+                        self.log.debug(
+                            f"{log_prefix} Закрытие висящего процесса с id={run.process_id}"
+                        )
+                        kill_proc_tree(run.process_id)
+
+            except (NoSuchProcess, AccessDenied):
+                pass
+
+            except Exception as e:
+                self.log.debug(
+                    f"{log_prefix} Ошибка при работе с процессом {run.process_id}: {e}"
+                )
+
+            self.log.debug(
+                f"{log_prefix} Установка запуску задачи (дата создания {run.create_date}) "
+                f"статус {TaskStatusEnum.Unknown.value}"
+            )
+            run.set_status(TaskStatusEnum.Unknown)
+
     def process(self):
         self.log.debug(f"{self._log_prefix} Запуск")
 
         while True:
-            task_runs: list[TaskRun] = self.owner.get_current_task_runs()
-            min_start_date: datetime = min(
-                [
-                    run.start_date
-                    for run in task_runs
-                    if run.get_actual_status() == TaskStatusEnum.Running
-                ],
-                default=datetime.now(),
-            )
-            # Небольшая фора
-            min_start_date -= timedelta(seconds=10)
-
-            # Разбирательства с "висячими" запусками
-            for run in TaskRun.select().where(
-                TaskRun.status == TaskStatusEnum.Running,
-                TaskRun.start_date < min_start_date,
-            ):
-                log_prefix = (
-                    f"{self._log_prefix} [Задача #{run.task.id}, запуск #{run.id}]"
-                )
-                try:
-                    # Попробуем найти процесс, если задан
-                    if run.process_id:
-                        p = Process(run.process_id)
-                        process_command: str = " ".join(p.cmdline())
-                        run_command = get_prefix_file_name_command(run.task, run)
-
-                        # Если процесс найден и в аргументах запуска есть часть названия батника
-                        if run_command in process_command:
-                            self.log.debug(
-                                f"{log_prefix} Закрытие висящего процесса с id={run.process_id}"
-                            )
-                            kill_proc_tree(run.process_id)
-
-                except (NoSuchProcess, AccessDenied):
-                    pass
-
-                except Exception as e:
-                    self.log.debug(
-                        f"{log_prefix} Ошибка при работе с процессом {run.process_id}: {e}"
-                    )
-
-                self.log.debug(
-                    f"{log_prefix} Установка запуску задачи (дата создания {run.create_date}) "
-                    f"статус {TaskStatusEnum.Unknown.value}"
-                )
-                run.set_status(TaskStatusEnum.Unknown)
+            self.__processing_hanging_runs()
 
             time.sleep(60)
