0c9a35849f243eef610270bf6b67d88cb368f043
gil9red <ilya.petrash@inbox.ru>
2024-03-13 17:04:15 +0500

    Обновлено. Вебсервер проверяет наличие запрашиваемых объектов и в случаи отсутствия возвращает 404

--- app_web/main.py
+++ app_web/main.py
@@ -4,7 +4,9 @@
 __author__ = "ipetrash"
 
 
-from flask import render_template, jsonify, Response
+from flask import render_template, jsonify, Response, abort
+
+from peewee import DoesNotExist
 
 from app_web import config
 from app_web.app import app
@@ -22,8 +24,10 @@ def index() -> str:
 
 @app.route("/task/<int:task_id>")
 def task(task_id: int) -> str:
-    # TODO: 404, если задача не найдена
-    task: Task = Task.get_by_id(task_id)
+    try:
+        task: Task = Task.get_by_id(task_id)
+    except DoesNotExist:
+        abort(404)
 
     return render_template(
         "task.html",
@@ -40,20 +44,23 @@ def api_tasks() -> Response:
 
 @app.route("/api/task/<int:task_id>/runs")
 def api_task_runs(task_id: int) -> Response:
-    # TODO: 404, если задача не найдена
-    task: Task = Task.get_by_id(task_id)
+    try:
+        task: Task = Task.get_by_id(task_id)
+    except DoesNotExist:
+        abort(404)
 
     return jsonify([obj.to_dict() for obj in task.runs.order_by(TaskRun.id)])
 
 
 @app.route("/api/task/<int:task_id>/run/<int:task_run_id>/logs")
 def api_task_run_logs(task_id: int, task_run_id: int) -> Response:
-    # TODO: 404, если не найдено
-    run: TaskRun = TaskRun.get_by_id(task_run_id)
+    try:
+        run: TaskRun = TaskRun.get_by_id(task_run_id)
+    except DoesNotExist:
+        abort(404)
 
-    # TODO: проверять родителя
-    # TODO: 404, если не найдено
-    # if run.task_id != task_id:
+    if run.task_id != task_id:
+        abort(404)
 
     return jsonify([obj.to_dict() for obj in run.logs.order_by(TaskRunLog.id)])
 
