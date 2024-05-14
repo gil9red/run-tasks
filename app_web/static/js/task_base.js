955e6430b9c6619b34ea3d023e012eb1852c9db0
gil9red <ilya.petrash@inbox.ru>
2024-05-14 13:29:42 +0500

    Обновление UI. Страница задачи. Добавлено поле расписания и даты последнего запуска

---
+++ app_web/static/js/task_base.js
@@ -0,0 +1,65 @@
+function process_cron() {
+    let $cron_result_error = $(".cron-result-error");
+    let $cron_result = $("#next-dates");
+
+    $cron_result_error.text("");
+    $cron_result.text("");
+
+    let cron = $.trim($("#cron").val());
+    if (!cron) {
+        return;
+    }
+
+    $.ajax({
+        url: "/api/cron/get-next-dates",
+        method: "GET",
+        data: {
+            cron: cron,
+        },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",  // Тип данных загружаемых с сервера
+        success: function(data) {
+            let ok = data.status == 'ok';
+            if (ok) {
+                let items = [];
+                for (let item of data.result) {
+                    let date_str = get_date_from_utc(item.date);
+                    items.push(
+                        `<div>${date_str}</div>`
+                    );
+                }
+                let result = items.join("");
+                $cron_result.html(result);
+            } else {
+                $cron_result_error.text(data.text);
+            }
+        },
+        error: data => on_ajax_error(data, "при запросе следующих дат")
+    });
+}
+
+
+$(function() {
+    process_cron();
+    $("#cron").on("input change", () => process_cron());
+
+    $('[type="checkbox"][data-bs-toggle="collapse"][data-bs-target]').each(
+        (i, item) => {
+            let $item = $(item);
+
+            if ($item.prop("checked")) {
+                bootstrap.Collapse
+                .getOrCreateInstance(
+                    $item.data("bs-target")
+                )
+                .show();
+            }
+        }
+    );
+
+    $(".cron-examples code").click(function() {
+        $("#cron").val(
+            $(this).text()
+        ).change();
+    });
+});
--- app_web/static/js/task_create_or_update.js
+++ app_web/static/js/task_create_or_update.js
@@ -1,49 +1,3 @@
-function get_cron() {
-    return $.trim($("#cron").val());
-}
-
-
-function process_cron() {
-    let $cron_result_error = $(".cron-result-error");
-    let $cron_result = $("#next-dates");
-
-    $cron_result_error.text("");
-    $cron_result.text("");
-
-    let cron = get_cron();
-    if (!cron) {
-        return;
-    }
-
-    $.ajax({
-        url: "/api/cron/get-next-dates",
-        method: "GET",
-        data: {
-            cron: cron,
-        },
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",  // Тип данных загружаемых с сервера
-        success: function(data) {
-            let ok = data.status == 'ok';
-            if (ok) {
-                let items = [];
-                for (let item of data.result) {
-                    let date_str = get_date_from_utc(item.date);
-                    items.push(
-                        `<div>${date_str}</div>`
-                    );
-                }
-                let result = items.join("");
-                $cron_result.html(result);
-            } else {
-                $cron_result_error.text(data.text);
-            }
-        },
-        error: data => on_ajax_error(data, "при запросе следующих дат")
-    });
-}
-
-
 function process_description() {
     let $description = $("#description");
     let $description_preview = $("#description-preview");
@@ -53,31 +7,8 @@ function process_description() {
 
 
 $(function() {
-    process_cron();
-    $("#cron").on("input change", () => process_cron());
-
     process_description();
     $("#description").on("input change", () => process_description());
-
-    $('[type="checkbox"][data-bs-toggle="collapse"][data-bs-target]').each(
-        (i, item) => {
-            let $item = $(item);
-
-            if ($item.prop("checked")) {
-                bootstrap.Collapse
-                .getOrCreateInstance(
-                    $item.data("bs-target")
-                )
-                .show();
-            }
-        }
-    );
-
-    $(".cron-examples code").click(function() {
-        $("#cron").val(
-            $(this).text()
-        ).change();
-    });
 
     $("form").submit(function() {
         let thisForm = this;
